d18feaaceb67414d756aef75fb0cea8f6b05013b
Md. Alamgir Hosen <64268610+fuck-boy-alam-vau-tera-papa@users.noreply.github.com>
2022-07-02 04:02:55 +0600

    Add files via upload

--- Dump.py
+++ Dump.py
@@ -1,34 +1,15 @@
-
-import platform
-import os
-os.system('termux-setup-storage')
-
-
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("Jm")._login()
-elif 'aarch' in arc:
-	__import__("Dump")._login()
-else:
-	exit(f' Unknow device machine {arc}')
-logo = ("""\033[1;32m
-       
-┌───────────────────────────────────────────────────────────┐
-│                                                           │
-│   █████╗ ██╗      █████╗ ███╗   ███╗ ██████╗ ██╗██████╗   │
-│  ██╔══██╗██║     ██╔══██╗████╗ ████║██╔════╝ ██║██╔══██╗  │
-│  ███████║██║     ███████║██╔████╔██║██║  ███╗██║██████╔╝  │
-│  ██╔══██║██║     ██╔══██║██║╚██╔╝██║██║   ██║██║██╔══██╗  │
-│  ██║  ██║███████╗██║  ██║██║ ╚═╝ ██║╚██████╔╝██║██║  ██║  │
-│  ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝╚═╝  ╚═╝  │
-│                                            │
-└───────────────────────────────────────────────────────────┘  \033
-__________________×______________________
-  
-  Auther   :  MD ALAMGIR HOSEN
- 
-  Github   :  THE DARK WORLD TEAM-CYBER ARMY&RECOVERY ZONE 
-  Facebook : https://www.facebook.com/TeraPaPa.ALAMgir.HoSeN ⭐⭐⭐⭐
-  
-  Contact : Whatsapp+8801712034653 
-__________________×______________________\033[1;37m""")
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from dump import sarfraz
+    sarfraz()
+elif bit == '32bit':
+    from SSB32 import ssbbuy
+    ssbbuy()
